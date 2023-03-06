ff58939d2c5147bcf52767021b0766685472e7e5
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-03-06 01:18:09 -0500

    Complete Lab

---
+++ index.js
@@ -0,0 +1,21 @@
+function introduction(name)
+{
+    return (`Hi, my name is ${name}.`);
+}
+
+function logTwoValues(value1, value2) 
+{
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+
+ function introductionWithLanguage(name, language)
+  {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+
+  function introductionWithLanguageOptional(name, language=`JavaScript`)
+  {
+
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+  
